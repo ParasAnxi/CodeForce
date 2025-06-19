4fc895c61f8e2acfe3a4325988e0416b38da730f
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-06-19 07:32:46 +0530

    1791B

---
+++ 1791B.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+
+bool solveC(string &st) {
+  int x = 0, y = 0;
+  for (auto ch : st) {
+    if (ch == 'L')
+      x--;
+    else if (ch == 'R')
+      x++;
+    else if (ch == 'U')
+      y++;
+    else if (ch == 'D')
+      y--;
+
+    if (x == 1 && y == 1) {
+      return 1;
+    }
+  }
+  return 0;
+}
+void solve() {
+  int n;
+  string st;
+  cin >> n >> st;
+
+  if (solveC(st)) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+}
+int main() {
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  // solve();
+  return 0;
+}
