3f20fec6bcb8b1ed275c5dacf715d6ab3181d462
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-06-23 16:20:10 +0530

    966A

---
+++ 996A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+
+void solve() {
+  ll n;
+  cin >> n;
+  ll ans = 0;
+  ans += n / 100;
+  n %= 100;
+  ans += n / 20;
+  n %= 20;
+  ans += n / 10;
+  n %= 10;
+  ans += n / 5;
+  n %= 5;
+  ans += n;
+  cout << ans << "\n";
+}
+int main() {
+  //   freopen("input.txt", "r", stdin);
+  //   freopen("output.txt", "w", stdout);
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  // int t;
+  // cin >> t;
+  // while (t--) {
+  //   solve();
+  // }
+  solve();
+  return 0;
+}
