9d3a3d573bd3d87e4350fb9d877691fd4fa59f12
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-07-24 09:32:52 +0530

    1348A

---
+++ 1348A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+
+void solve() {
+  int n;
+  cin >> n;
+  ll s1 = 0, s2 = 0;
+  s1 += (1LL << n);
+  for (int i = 1; i < n / 2; i++) {
+    s1 += (1LL << i);
+  }
+  for (int i = n / 2; i < n; i++) {
+    s2 += (1LL << i);
+  }
+  cout << llabs(s1 - s2) << "\n";
+}
+int main() {
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
