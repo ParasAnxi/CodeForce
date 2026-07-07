07498a27e8d28ddc84d500d578137b35740948d0
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-07-07 21:55:24 +0530

    1480A

---
+++ 1480A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MAX_N 200001
+#define MAX_L 18
+#define MOD 1000000007
+#define INF 1000000000LL
+#define LMOD 1000000000000000007LL
+
+void solve() {
+  string s;
+  cin >> s;
+  int n = s.length();
+  for (int i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+      if (s[i] != 'a') {
+        s[i] = 'a';
+      } else {
+        s[i] = 'b';
+      }
+    } else {
+      if (s[i] != 'z') {
+        s[i] = 'z';
+      } else {
+        s[i] = 'y';
+      }
+    }
+  }
+  cout << s << "\n";
+}
+int main() {
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  // solve();
+  return 0;
+}
