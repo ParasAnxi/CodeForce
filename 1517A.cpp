7e383c0f8bd6058eb1b0c58dd82ebdad00add707
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-07-13 22:36:36 +0530

    1517A

---
+++ 1517A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MAX_N 200001
+#define MAX_L 18
+#define MOD 1000000007
+#define INF 1000000000LL
+#define LMOD 1000000000000000007LL
+
+void solve() {
+  ll n;
+  cin >> n;
+  if (n % 2050 != 0) {
+    cout << -1 << "\n";
+    return;
+  }
+  ll q = n / 2050;
+  ll ans = 0;
+  while (q > 0) {
+    ans += q % 10;
+    q /= 10;
+  }
+  cout << ans << "\n";
+}
+int main() {
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  // solve();
+  return 0;
+}
