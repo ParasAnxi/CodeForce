3b96d014660c90a20d16b8ccdcc28136e4b79fce
Paras Bhandari <paras.bhandari7010@gmail.com>
2026-09-11 13:57:12 +0530

    1886A

---
+++ 1886A.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MAX_N 200001
+#define MAX_L 18
+#define MOD 1000000007
+#define INF 1000000000LL
+#define LMOD 1000000000000000007LL
+
+void solve() {
+  ll n;
+  cin >> n;
+  if (n % 3 != 0) {
+    ll x = 1, y = 2, z = n - 3;
+    if (z > 0 && z != x && z != y) {
+      cout << "YES" << "\n";
+      cout << x << " " << y << " " << z << "\n";
+    } else {
+      cout << "NO" << "\n";
+    }
+  }
+  else {
+    ll x = 1, y = 4, z = n - 5;
+    if (z > 0 && z != x && z != y && z % 3 != 0) {
+      cout << "YES" << "\n";
+      cout << x << " " << y << " " << z << "\n";
+    } else {
+      cout << "NO" << "\n";
+    }
+  }
+}
+int main() {
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  // solve();
+  return 0;
+}
